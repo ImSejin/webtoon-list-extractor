3ce2614db48a2e646a682edab8e3a7e729938dd3
ImSejin <hackeres123@github.com>
2020-08-20 21:19:29 +0900

    🔖 Release: v1.1.13

--- src/main/java/io/github/imsejin/common/ApplicationMetadata.java
+++ src/main/java/io/github/imsejin/common/ApplicationMetadata.java
@@ -11,7 +11,7 @@ public final class ApplicationMetadata {
 
     public static final String APPLICATION_NAME = "WebtoonListExtractor";
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.13";
 
     public static final String VERSION_NAME = "v" + VERSION + ".RELEASE";
 
