45ca9d5c3362d9a316ddf7dd4cd9bb3b4d65ee46
ImSejin <hackeres123@gmail.com>
2020-11-11 19:41:36 +0900

    📝 Update: version --- 2.0.1

--- src/main/java/io/github/imsejin/wnliext/common/ApplicationMetadata.java
+++ src/main/java/io/github/imsejin/wnliext/common/ApplicationMetadata.java
@@ -7,7 +7,7 @@ public final class ApplicationMetadata {
 
     public static final String APPLICATION_NAME = "WebtoonListExtractor";
 
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.1";
 
     public static final String[] APPLICATION_TITLE = { "	                 _                     _    _     _",
                                                 "	___ __   ____   | |                   | |  (_)   | |",
