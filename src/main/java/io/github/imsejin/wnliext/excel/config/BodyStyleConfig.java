aba78825f2a842aafb21458ffd6389dd137f07b3
ImSejin <hackeres123@gmail.com>
2020-11-11 15:12:59 +0900

    ✨ Add: excel style configurations

---
+++ src/main/java/io/github/imsejin/wnliext/excel/config/BodyStyleConfig.java
@@ -0,0 +1,20 @@
+package io.github.imsejin.wnliext.excel.config;
+
+import com.github.javaxcel.styler.ExcelStyleConfig;
+import com.github.javaxcel.styler.config.Configurer;
+import org.apache.poi.ss.usermodel.*;
+
+public class BodyStyleConfig implements ExcelStyleConfig {
+
+    @Override
+    public void configure(Configurer configurer) {
+        configurer.border()
+                    .all(BorderStyle.THIN, IndexedColors.BLACK)
+                .and()
+                .font()
+                    .name("NanumGothic")
+                    .size(10)
+                    .color(IndexedColors.BLACK);
+    }
+
+}
---
+++ src/main/java/io/github/imsejin/wnliext/excel/config/RightBodyStyleConfig.java
@@ -0,0 +1,27 @@
+package io.github.imsejin.wnliext.excel.config;
+
+import com.github.javaxcel.styler.ExcelStyleConfig;
+import com.github.javaxcel.styler.config.Configurer;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+
+public class RightBodyStyleConfig implements ExcelStyleConfig {
+
+    @Override
+    public void configure(Configurer configurer) {
+        configurer.alignment()
+                    .horizontal(HorizontalAlignment.RIGHT)
+                    .vertical(VerticalAlignment.CENTER)
+                    .and()
+                .border()
+                    .all(BorderStyle.THIN, IndexedColors.BLACK)
+                .and()
+                .font()
+                    .name("NanumGothic")
+                    .size(10)
+                    .color(IndexedColors.BLACK);
+    }
+
+}
